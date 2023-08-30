2e5e5b578f9a4cc322ea96b80ab7dfeb11211496
matheuscola15 <matheuscola295@gmail.com>
2023-08-30 08:55:15 -0300

    class 30/08

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,5 +1,24 @@
-export default function Cardapio(){
+import { useState } from 'react'
+import Buscador from './Buscador';
+import styles from './Cardapio.module.scss'
+import logo from '/reactproject/src/assets/logo.png'
+
+export default function Cardapio() {
+    const [busca, setBusca] = useState("");
     return(
-        <div>Cardapio de Restaurantes do bola</div>
-    );
+        <main>
+            <nav className={styles.menu}>
+                <img src={logo} className={styles.logo} alt="logo" />
+            </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    Cola
+                </div>
+            </header>
+            <section>
+                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+                <Buscador busca={busca} setBusca={setBusca}/>
+            </section>
+        </main>
+    )
 }
